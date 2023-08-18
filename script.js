8d8136fa1deaf92b74898ffc29e903379cdd3176
matthewrboland <matthewrboland@gmail.com>
2023-08-17 19:57:14 -0700

    Update script.js V3 - multi-picker and custom

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const generateButton = document.getElementById('generate-btn');
-    const problemSelect = document.getElementById('problem-select');
-    const solutionSelect = document.getElementById('solution-select');
+    const problemCheckboxes = document.getElementById('problem-checkboxes');
+    const solutionCheckboxes = document.getElementById('solution-checkboxes');
     const customProblemInput = document.getElementById('custom-problem');
     const customSolutionInput = document.getElementById('custom-solution');
+    const addCustomProblemButton = document.getElementById('add-custom-problem');
+    const addCustomSolutionButton = document.getElementById('add-custom-solution');
     const resultDiv = document.getElementById('result');
 
     // Load problems from "problems.txt"
@@ -11,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.text())
         .then(data => {
             const problems = data.split('\n');
-            populateSelect(problemSelect, problems);
+            populateCheckboxes(problemCheckboxes, problems);
         });
 
     // Load solutions from "solutions.txt"
@@ -19,23 +21,31 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.text())
         .then(data => {
             const solutions = data.split('\n');
-            populateSelect(solutionSelect, solutions);
+            populateCheckboxes(solutionCheckboxes, solutions);
         });
 
-    function populateSelect(selectElement, options) {
+    function populateCheckboxes(checkboxesContainer, options) {
         for (const option of options) {
             if (option.trim() !== '') {
-                const optionElement = document.createElement('option');
-                optionElement.value = option;
-                optionElement.textContent = option;
-                selectElement.appendChild(optionElement);
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.value = option;
+                checkbox.id = option;
+                
+                const label = document.createElement('label');
+                label.textContent = option;
+                label.setAttribute('for', option);
+                
+                checkboxesContainer.appendChild(checkbox);
+                checkboxesContainer.appendChild(label);
+                checkboxesContainer.appendChild(document.createElement('br'));
             }
         }
     }
 
     generateButton.addEventListener('click', function() {
-        const selectedProblems = getSelectedValues(problemSelect);
-        const selectedSolutions = getSelectedValues(solutionSelect);
+        const selectedProblems = getSelectedValues(problemCheckboxes);
+        const selectedSolutions = getSelectedValues(solutionCheckboxes);
         
         if (selectedProblems.length === 0 || selectedSolutions.length === 0) {
             resultDiv.textContent = 'Please select at least one problem and one solution.';
@@ -48,11 +58,54 @@ document.addEventListener('DOMContentLoaded', function() {
         resultDiv.textContent = randomProblem + ' - ' + randomSolution;
     });
 
-    function getSelectedValues(selectElement) {
+    addCustomProblemButton.addEventListener('click', function() {
+        const customProblem = customProblemInput.value.trim();
+        if (customProblem !== '') {
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.value = customProblem;
+            checkbox.id = customProblem;
+            checkbox.checked = true;
+            
+            const label = document.createElement('label');
+            label.textContent = customProblem;
+            label.setAttribute('for', customProblem);
+            
+            problemCheckboxes.appendChild(checkbox);
+            problemCheckboxes.appendChild(label);
+            problemCheckboxes.appendChild(document.createElement('br'));
+            
+            customProblemInput.value = '';
+        }
+    });
+
+    addCustomSolutionButton.addEventListener('click', function() {
+        const customSolution = customSolutionInput.value.trim();
+        if (customSolution !== '') {
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.value = customSolution;
+            checkbox.id = customSolution;
+            checkbox.checked = true;
+            
+            const label = document.createElement('label');
+            label.textContent = customSolution;
+            label.setAttribute('for', customSolution);
+            
+            solutionCheckboxes.appendChild(checkbox);
+            solutionCheckboxes.appendChild(label);
+            solutionCheckboxes.appendChild(document.createElement('br'));
+            
+            customSolutionInput.value = '';
+        }
+    });
+
+    function getSelectedValues(checkboxesContainer) {
         const selectedValues = [];
-        for (let i = 0; i < selectElement.options.length; i++) {
-            if (selectElement.options[i].selected) {
-                selectedValues.push(selectElement.options[i].value);
+        const checkboxes = checkboxesContainer.querySelectorAll('input[type="checkbox"]');
+        for (const checkbox of checkboxes) {
+            if (checkbox.checked) {
+                selectedValues.push(checkbox.value);
             }
         }
         return selectedValues;
