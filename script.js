817465a762f0b332caa0ffccbc5ab4c754e329fd
matthewrboland <matthewrboland@gmail.com>
2023-08-17 19:42:20 -0700

    Update script.js V2
    Includes list

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
     const generateButton = document.getElementById('generate-btn');
+    const problemSelect = document.getElementById('problem-select');
+    const solutionSelect = document.getElementById('solution-select');
+    const customProblemInput = document.getElementById('custom-problem');
+    const customSolutionInput = document.getElementById('custom-solution');
     const resultDiv = document.getElementById('result');
-    
+
+    // Load problems from "problems.txt"
+    fetch('problems.txt')
+        .then(response => response.text())
+        .then(data => {
+            const problems = data.split('\n');
+            populateSelect(problemSelect, problems);
+        });
+
+    // Load solutions from "solutions.txt"
+    fetch('solutions.txt')
+        .then(response => response.text())
+        .then(data => {
+            const solutions = data.split('\n');
+            populateSelect(solutionSelect, solutions);
+        });
+
+    function populateSelect(selectElement, options) {
+        for (const option of options) {
+            if (option.trim() !== '') {
+                const optionElement = document.createElement('option');
+                optionElement.value = option;
+                optionElement.textContent = option;
+                selectElement.appendChild(optionElement);
+            }
+        }
+    }
+
     generateButton.addEventListener('click', function() {
-        const list1 = document.getElementById('list1').value;
-        const list2 = document.getElementById('list2').value;
+        const selectedProblem = problemSelect.value || customProblemInput.value;
+        const selectedSolution = solutionSelect.value || customSolutionInput.value;
         
-        const list1Array = list1.split('\n');
-        const list2Array = list2.split('\n');
-        
-        if (list1Array.length === 0 || list2Array.length === 0) {
-            resultDiv.textContent = 'Please enter both lists.';
+        if (!selectedProblem || !selectedSolution) {
+            resultDiv.textContent = 'Please select or enter both a problem and a solution.';
             return;
         }
         
-        const randomItem1 = list1Array[Math.floor(Math.random() * list1Array.length)];
-        const randomItem2 = list2Array[Math.floor(Math.random() * list2Array.length)];
-        
-        resultDiv.textContent = randomItem1 + ' - ' + randomItem2;
+        resultDiv.textContent = selectedProblem + ' - ' + selectedSolution;
     });
 });
